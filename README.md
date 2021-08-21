4602e511c7c6068dedfed528ab553901f06491c4
GabrielCastilloM <jgabrielcastillo@gmail.com>
2021-08-20 22:17:20 -0500

    creacion archivos promedio

--- README.md
+++ README.md
@@ -10,7 +10,15 @@
 - Cuarto paso: integrar JS con HTML
 - Quinto paso dar estilos con css
 
-## Taller #21: porcentajes y descuentos
+## Taller #2: porcentajes y descuentos
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implemetra formulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
+- Quinto paso dar estilos con css
+
+## Taller #3: Promedio, mediana y moda
 
 - Primer paso: definir las fórmulas
 - Segundo paso: implemetra formulas en JavaScript
--- descuentos.js
+++ descuentos.js
@@ -1,12 +1,10 @@
 //const precioOriginal = 120;
 //const descuento = 18;
-
 function calcularPrecioConDescuento (precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
 }
-
 
 function aplicarDescuento() {
     const inputPrice = document.querySelector("#inputPrice")
@@ -15,36 +13,32 @@ function aplicarDescuento() {
     const inputDiscount = document.querySelector("#inputDiscount")
     const discountValue = parseInt(inputDiscount.value)
 
-    const cuponAplicado = obtenerCupon()    
-
+    const cuponAplicado = obtenerCupon()
     const descuentoMasCupon = parseInt(cuponAplicado + discountValue)
-   
+
     const precioConDescuento = calcularPrecioConDescuento(priceValue, descuentoMasCupon)
     const resultp = document.querySelector("#resultP")
     resultp.innerText = 'El precio con descuento es: $' + precioConDescuento;
     pintarResultado()
-
 }
 
 function obtenerCupon(){
-    /* Para obtener el valor */
+    /* Para obtener el valor de un select*/
     var cupon = parseInt(document.querySelector(".form-select").value);
     return cupon
 }
+
 function pintarResultado() {
-    const estiloResultado = document.querySelector(".card-footer")    
+    const estiloResultado = document.querySelector(".card-footer")
     console.log(estiloResultado);
     estiloResultado.style = "background:#157347";
-    
 }
-
 
         // /* Para obtener el texto */
         // var combo = document.querySelector(".form-select");
         // var selected = combo.options[combo.selectedIndex].text;
         // alert(selected);
         // }
-
 
 // console.log({
 //     precioOriginal,
